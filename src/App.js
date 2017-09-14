ee5bfd062a200ac7be6dfd13cf7a0a492b1c1a89
surya-durgaputra <surya.durgaputra@gmail.com>
2017-09-14 12:28:56 +0400

    style drawer links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {BrowserRouter, Route, Link} from 'react-router-dom'
+import {BrowserRouter, Route, NavLink} from 'react-router-dom'
 import './App.css'
 import {AppBar, Drawer,MenuItem} from 'material-ui'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
@@ -14,7 +14,8 @@ class App extends Component {
     super(props)
     this.state = {open:false}
   }
-  handleToggle = () => this.setState({open:!this.state.open})
+  handleClose = () => this.setState({open: false});
+  handleToggle = () => this.setState({open: !this.state.open});
   render() {
     return (
     <BrowserRouter>
@@ -30,27 +31,27 @@ class App extends Component {
             docked={false}
             onRequestChange={(open) => this.setState({open})}
           >
-            <MenuItem>
-              <Link to='/'>Home</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to='/logos'>Logos</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to='/posters'>Posters</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to='/letterhead'>Letterhead</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to='/share-a-story'>Share a Story</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to='/service-request-form'>Service Request Form</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to='/tutorial'>Tutorial</Link>
-            </MenuItem>
+            <NavLink to='/'>
+              <MenuItem onTouchTap={this.handleClose}>Home</MenuItem>
+            </NavLink>
+            <NavLink to='/logos'>
+              <MenuItem onTouchTap={this.handleClose}>Logos</MenuItem>
+            </NavLink>
+            <NavLink to='/posters'>
+              <MenuItem onTouchTap={this.handleClose}>Posters</MenuItem>
+            </NavLink>
+            <NavLink to='/letterhead'>
+              <MenuItem onTouchTap={this.handleClose}>Letterhead</MenuItem>
+            </NavLink>
+            <NavLink to='/share-a-story'>
+              <MenuItem onTouchTap={this.handleClose}>Share a Story</MenuItem>
+            </NavLink>
+            <NavLink to='/service-request-form'>
+              <MenuItem onTouchTap={this.handleClose}>Service Request Form</MenuItem>
+            </NavLink>
+            <NavLink to='/tutorial'>
+              <MenuItem onTouchTap={this.handleClose}>Tutorial</MenuItem>
+            </NavLink>
           </Drawer>
           <Route exact path='/' render={(props)=>{
               return <h1>Home View</h1>
