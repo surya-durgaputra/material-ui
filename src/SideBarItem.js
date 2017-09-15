d5705bb2f6fa025fc8b26f538691bddf96b63856
surya-durgaputra <surya.durgaputra@gmail.com>
2017-09-15 10:35:47 +0400

    made SideBarItem a stateless functional component

--- src/SideBarItem.js
+++ src/SideBarItem.js
@@ -2,24 +2,30 @@ import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 import { MenuItem } from 'material-ui'
 
-class SideBarItem extends Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    return (
-      <NavLink
-        exact={this.props.isExact}
-        to={this.props.link}
-        activeClassName="active"
-      >
-        <MenuItem
-          onTouchTap={this.props.handleClose}
-          primaryText={this.props.text}
-        />
-      </NavLink>
-    )
-  }
-}
+const SideBarItem = props => (
+  <NavLink exact={props.isExact} to={props.link} activeClassName="active">
+    <MenuItem onTouchTap={props.handleClose} primaryText={props.text} />
+  </NavLink>
+)
+
+// class SideBarItem extends Component {
+//   constructor(props) {
+//     super(props)
+//   }
+//   render() {
+//     return (
+//   <NavLink
+//     exact={this.props.isExact}
+//     to={this.props.link}
+//     activeClassName="active"
+//   >
+//     <MenuItem
+//       onTouchTap={this.props.handleClose}
+//       primaryText={this.props.text}
+//     />
+//   </NavLink>
+//     )
+//   }
+// }
 
 export default SideBarItem
