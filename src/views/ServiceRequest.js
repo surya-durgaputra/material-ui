e673855933c76ad9d414c59dea5393b9be795eaa
surya-durgaputra <surya.durgaputra@gmail.com>
2017-09-19 13:48:42 +0400

    added service request form fields

--- src/views/ServiceRequest.js
+++ src/views/ServiceRequest.js
@@ -1,8 +1,171 @@
 import React, { Component } from 'react'
+import Checkbox from 'material-ui/Checkbox'
+import TextField from 'material-ui/TextField'
+import DatePicker from 'material-ui/DatePicker'
+import MaterializeRaisedButton from '../components/MaterializeRaisedButton'
+import '../styles/responsive-text.css'
+
+const styles = {
+  block: {
+    maxWidth: 250
+  },
+  checkbox: {
+    marginBottom: 16
+  }
+}
 
 class ServiceRequest extends Component {
   render() {
-    return <h1>Service Request Form View</h1>
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col s12 flow-text">
+            <h2>Please use this form to request services</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12 m6 flow-text">
+            <TextField
+              floatingLabelText="Floating Label Text"
+              fullWidth={true}
+            />
+            <br />
+          </div>
+          <div className="col s12 m6 flow-text">
+            <TextField
+              floatingLabelText="Floating Label Text"
+              fullWidth={true}
+            />
+            <br />
+          </div>
+          <div className="col s12 m6 flow-text">
+            <TextField
+              floatingLabelText="Floating Label Text"
+              fullWidth={true}
+            />
+            <br />
+          </div>
+          <div className="col s12 m6 flow-text">
+            <TextField
+              floatingLabelText="Floating Label Text"
+              fullWidth={true}
+            />
+            <br />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12 m6">
+            <TextField
+              floatingLabelText="Multiline and Floating Label Text"
+              multiLine={true}
+              rows={2}
+              fullWidth={true}
+            />
+            <br />
+          </div>
+          <div className="col s12 m6">
+            <TextField
+              floatingLabelText="Multiline and Floating Label Text"
+              multiLine={true}
+              rows={2}
+              fullWidth={true}
+            />
+            <br />
+          </div>
+          <div className="col s12 m6">
+            <TextField
+              floatingLabelText="Multiline and Floating Label Text"
+              multiLine={true}
+              rows={2}
+              fullWidth={true}
+            />
+            <br />
+          </div>
+          <div className="col s12 m6">
+            <TextField
+              floatingLabelText="Multiline and Floating Label Text"
+              multiLine={true}
+              rows={2}
+              fullWidth={true}
+            />
+            <br />
+          </div>
+          <div className="col s12 m6">
+            <TextField
+              floatingLabelText="Multiline and Floating Label Text"
+              multiLine={true}
+              rows={2}
+              fullWidth={true}
+            />
+            <br />
+          </div>
+          <div className="col s12 m6">
+            <TextField
+              floatingLabelText="Multiline and Floating Label Text"
+              multiLine={true}
+              rows={2}
+              fullWidth={true}
+            />
+            <br />
+          </div>
+          <div className="col s12 m6">
+            <TextField
+              floatingLabelText="Multiline and Floating Label Text"
+              multiLine={true}
+              rows={2}
+              fullWidth={true}
+            />
+            <br />
+          </div>
+          <div className="col s12 m6">
+            <TextField
+              floatingLabelText="Multiline and Floating Label Text"
+              multiLine={true}
+              rows={2}
+              fullWidth={true}
+            />
+            <br />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col s12 m6">
+            <DatePicker hintText="Portrait Dialog" />
+          </div>
+          <div className="col s12 m6">
+            <DatePicker hintText="File upload placeholder" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12 m6">
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+          </div>
+          <div className="col s12 m6">
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            <MaterializeRaisedButton label="Submit" primary={true} />
+            <Checkbox label="Simple" style={styles.checkbox} />
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
